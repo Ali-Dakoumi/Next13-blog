b613691fe05a8cbd5730e1d3755a8e00bd22989b
Ali Dakoumi <dakoumi31@gmail.com>
2023-07-06 13:00:34 +0100

    adding MUI

--- components/Article/PreviewArticle.js
+++ components/Article/PreviewArticle.js
@@ -1,18 +1,36 @@
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
-import Link from "next/link";
+import { Link, Card, Button, CardActions, CardContent, CardMedia, Typography } from "../MUI/Proxy/index"
 
-export const PreviewArticle = ({ 
+
+export const PreviewArticle = ({
   uid,
   title,
   article_image,
-  author: { data: { name: [{ text : author_name }] } } 
-  }) => (
-  <div className="py-2 font-medium text-white bg-theme-heading bannerLink">
-    Preview
-    <p>{author_name}</p>
-    <Link href={`/articles/${uid}`}><PrismicRichText field={title} /></Link>
-    <Link href={`/articles/${uid}`}><PrismicNextImage field={article_image} /></Link>
-  </div>
+  author: { data: { name: [{ text: author_name }] } }
+}) => (
+  <Card sx={{ maxWidth: 345 }} width="100%" height="100%">
+    <Link underline="none" href={`/articles/${uid}`}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={article_image.url}
+      />
+    </Link>
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        <Link color="inherit" underline="none" href={`/articles/${uid}`}>
+          <PrismicRichText field={title} />
+        </Link>
+      </Typography>
+      <Typography gutterBottom variant="p" component="div">
+        {author_name}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Link color="inherit" underline="none" href={`/articles/${uid}`}>
+        <Button size="small">Read More</Button>
+      </Link>
+    </CardActions>
+  </Card>
 );
 
